717838829c1ee898868d2fe3381da7e4e0445407
mtrey0220 <111877570+mtrey0220@users.noreply.github.com>
2024-03-16 12:20:33 -0500

    Word Grid Design Update

---
+++ src/main/java/uta/cse3310/WordGrid.java
@@ -0,0 +1,64 @@
+package uta.cse3310;
+
+public class WordGrid{
+	
+	public char[][] grid;
+	public String[] wordList;
+    public int gridSize;
+    public Random random;
+	
+	public WordGrid(){
+
+        // Constructor \\
+
+	}
+
+    public void initializeGrid() {
+
+        // Method to Generate Grid w/ spaces. \\
+
+    }
+
+    public void displayGrid() {
+
+        // Method to display Grid. \\
+
+    }
+	
+	public boolean SelectionValidation(){
+
+        // Method to ensure the Word selected is in the WordBank/WordList. \\
+		return false;
+	}
+	
+	public boolean checkHorizontal(){
+
+        // Method for checking words Horizontally. \\
+		return false;
+	}
+	
+	public boolean checkVertical(){
+
+        // Method for checking words Vertically. \\
+		return false;
+	}
+	
+	public boolean checkDiagonal(){
+
+        // Method for checking words Diagonally. \\
+		return false;
+	}
+	
+	public String[] generateWordList(){
+
+        // Method for generating the Word Bank. \\
+		return wordList;
+	}
+	
+	public char[][] fillGrid(){
+
+        // Method for filling in the Grid w/ random letters \\
+		return grid;
+	}
+	
+}
