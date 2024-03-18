5956dbc0cbce8c603d534756b2efb995943b8ff7
mtrey0220 <111877570+mtrey0220@users.noreply.github.com>
2024-03-17 23:02:38 -0500

    Server Class Design

--- src/main/java/uta/cse3310/HttpServer.java
+++ src/main/java/uta/cse3310/HttpServer.java
@@ -11,43 +11,22 @@ import net.freeutils.httpserver.HTTPServer.Request;
 import net.freeutils.httpserver.HTTPServer.Response;
 import net.freeutils.httpserver.HTTPServer.VirtualHost;
 
-// http server include is a GPL licensed package from
-//            http://www.freeutils.net/source/jlhttp/
-
 public class HttpServer {
 
+    // Directory where HTML files are stored \\
     private static final String HTML = "./html";
     int port = 8080;
     String dirname = HTML;
 
+    // Constructor to initialize the HttpServer \\
     public HttpServer(int portNum, String dirName) {
-        port = portNum;
-        dirname = dirName;
+        // Initialize port number and directory name \\
+        this.port = portNum;
+        this.dirname = dirName;
     }
 
+    
     public void start() {
-        try {
-            File dir = new File(dirname);
-            if (!dir.canRead())
-                throw new FileNotFoundException(dir.getAbsolutePath());
-            // set up server
-            HTTPServer server = new HTTPServer(port);
-            VirtualHost host = server.getVirtualHost(null); // default host
-            host.setAllowGeneratedIndex(true); // with directory index pages
-            host.addContext("/", new FileContextHandler(dir));
-            host.addContext("/api/time", new ContextHandler() {
-                public int serve(Request req, Response resp) throws IOException {
-                    long now = System.currentTimeMillis();
-                    resp.getHeaders().add("Content-Type", "text/plain");
-                    resp.send(200, String.format("%tF %<tT", now));
-                    return 0;
-                }
-            });
-            server.start();
-        } catch (Exception e) {
-            System.err.println("error: " + e);
-        }
-
+        // Implementation for starting the HTTP server added here
     }
-
 }
