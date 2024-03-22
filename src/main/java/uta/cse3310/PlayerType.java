462e162e08349394bce3254293e9fb58b1e4c366
EbenezerB <122925564+EbenezerB@users.noreply.github.com>
2024-03-22 15:45:10 -0500

    Update PlayerType.java

--- src/main/java/uta/cse3310/PlayerType.java
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -1,43 +1,88 @@
 package uta.cse3310;
-
 public enum PlayerType {
-    NOPLAYER {
+    
+       NOPLAYER {
         @Override
-        public void method1() {
+        public int getScore() {
        return 0;
         }
 
         @Override
-        public void method2() {
-           return 0;
+        public void updateScore(int newScore) {
+         
         }
+        
     },
     XPLAYER {
+        private String nickname;
+        private int playercurrScore;
+        private int playerID;
+        
         @Override
-        public void method1() {
-         return 0;
+        public int getScore() {
+            
+         return playercurrScore;
+          
         }
 
         @Override
-        public void method2() {
-    return 0;
+        public void updateScore(int newScore) {
+   
         }
+        
+        public String getNickname(){
+            return nickname;
+        }
+        public void setNickname(){
+            
+        }
+        
+        public int getPlayerID(){
+            return playerID;
+        }
+        public void setPlayerID(int playerID){
+            
+        }
+        
+        
     },
     OPLAYER {
+        private String nickname;
+        private int playercurrScore;
+        private int playerID;
+        
         @Override
-        public void method1() {
-           return 0;
+        public int getScore() {
+            
+         return playercurrScore;
+          
         }
 
         @Override
-        public void method2() {
-        return 0;
+        public void updateScore(int newScore) {
+   
+        }
+        
+        public String getNickname(){
+            return nickname;
+        }
+        public void setNickname(){
+            
+        }
+        
+        public int getPlayerID(){
+            return playerID;
+        }
+        public void setPlayerID(int playerID){
+            
         }
     };
-
     
-    PlayerType() {
+    PlayerType(){
     }
-    public abstract void method1();
-    public abstract void method2();
-}
+    
+        public abstract int getScore();
+        
+        public abstract void updateScore(int newScore);
+    }
+    
