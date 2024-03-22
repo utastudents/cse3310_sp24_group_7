9409789c2d3a2d822a505219a141c644bcbb0539
EbenezerB <122925564+EbenezerB@users.noreply.github.com>
2024-03-22 10:08:02 -0500

    Update PlayerType.java

--- src/main/java/uta/cse3310/PlayerType.java
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -4,43 +4,40 @@ public enum PlayerType {
     NOPLAYER {
         @Override
         public void method1() {
-            // Empty method for NOPLAYER
+       return 0;
         }
 
         @Override
         public void method2() {
-           
+           return 0;
         }
     },
     XPLAYER {
         @Override
         public void method1() {
-            // Empty method for XPLAYER
+         return 0;
         }
 
         @Override
         public void method2() {
-    
+    return 0;
         }
     },
     OPLAYER {
         @Override
         public void method1() {
-            // Empty method for OPLAYER
+           return 0;
         }
 
         @Override
         public void method2() {
-        
+        return 0;
         }
     };
 
-    // Constructor
+    
     PlayerType() {
-      
     }
-
-
     public abstract void method1();
     public abstract void method2();
 }
