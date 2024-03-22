e99e8f613978554b0f0b4676390fd4f319c20d12
Abubakar Kassim <142273851+RSG0@users.noreply.github.com>
2024-03-22 18:44:17 -0500

    Add files via upload
    Imported Player lobby

---
+++ src/main/java/uta/cse3310/LobbyScreen.java
@@ -0,0 +1,39 @@
+//package cse.3310;
+
+public class LobbyScreen 
+{
+    public void startGame()
+    {
+        return;
+    }
+    public void createLobby()
+    {
+        
+    }
+    public void waitingLobby()
+    {
+        return;
+    }
+    public void addPlayer(String nickname)
+    {
+        //Adding a player and displaying their information
+        return;
+    }
+    public void removePlayer(String nickname)
+    {
+        return;
+    }
+    public String enterMessage(String message)
+    {
+        return;
+    }
+    public String displayMessage (String nickanme, String message)
+    {
+        //Display the username with the following information
+        return "message";
+        /*
+         * displayMessage("RSG", "Nice Score"):
+         * EX: RSG: Nice Score
+         */
+    }
+}
