33dde5165783401f8974098e8fda704ffcad657d
bcg2488 <bcg2488@mavs.uta.edu>
2024-03-22 19:17:14 -0500

    GameScreen Class

---
+++ src/main/java/uta/cse3310/GameScreen.java
@@ -0,0 +1,39 @@
+package uta.cse3310;
+
+public class GameScreen {
+
+    public String[] Msg;
+    public int GameId;
+
+    public void StartGarme() {
+        return;
+    }
+
+    public void Update() {
+        return;
+    }
+
+    public void CheckWord() {
+        return;
+    }
+
+    public void CheckVertical() {
+        return;
+    }
+
+    public void CheckHorizontal() {
+        return;
+    }
+
+    public void CheckDiagonal() {
+        return;
+    }
+
+    public void CheckReverse() {
+        return;
+    }
+
+    public void Tick() {
+        return;
+    }
+}
