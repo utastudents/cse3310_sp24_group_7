d5c11e8295c820f9a0c0bbecc74b65068323c72d
EbenezerB <122925564+EbenezerB@users.noreply.github.com>
2024-04-10 20:56:12 +0000

    PlayerType

--- src/main/java/uta/cse3310/PlayerType.java
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -1,88 +1,87 @@
 package uta.cse3310;
+
 public enum PlayerType {
-    
-       NOPLAYER {
+
+    NOPLAYER {
         @Override
         public int getScore() {
-       return 0;
+            return 0;
         }
 
         @Override
         public void updateScore(int newScore) {
-         
+            // Do nothing for NOPLAYER
         }
-        
     },
+
     XPLAYER {
         private String nickname;
         private int playercurrScore;
         private int playerID;
-        
+
         @Override
         public int getScore() {
-            
-         return playercurrScore;
-          
+            return playercurrScore;
         }
 
         @Override
         public void updateScore(int newScore) {
-   
+            playercurrScore += newScore;
         }
-        
-        public String getNickname(){
+
+        public String getNickname() {
             return nickname;
         }
-        public void setNickname(){
-            
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
         }
-        
-        public int getPlayerID(){
+
+        public int getPlayerID() {
             return playerID;
         }
-        public void setPlayerID(int playerID){
-            
+
+        public void setPlayerID(int playerID) {
+            this.playerID = playerID;
         }
-        
-        
     },
+
     OPLAYER {
         private String nickname;
         private int playercurrScore;
         private int playerID;
-        
+
         @Override
         public int getScore() {
-            
-         return playercurrScore;
-          
+            return playercurrScore;
         }
 
         @Override
         public void updateScore(int newScore) {
-   
+            playercurrScore += newScore;
         }
-        
-        public String getNickname(){
+
+        public String getNickname() {
             return nickname;
         }
-        public void setNickname(){
-            
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
         }
-        
-        public int getPlayerID(){
+
+        public int getPlayerID() {
             return playerID;
         }
-        public void setPlayerID(int playerID){
-            
+
+        public void setPlayerID(int playerID) {
+            this.playerID = playerID;
         }
     };
-    
-    PlayerType(){
+
+    PlayerType() {
     }
-    
-        public abstract int getScore();
-        
-        public abstract void updateScore(int newScore);
-    }
-    
+
+    public abstract int getScore();
+
+    public abstract void updateScore(int newScore);
+}
