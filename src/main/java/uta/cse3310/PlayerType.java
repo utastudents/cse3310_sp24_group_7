1e3cb304f20f7dc7b963fa909ccf2a18245d7086
mtrey0220 <111877570+mtrey0220@users.noreply.github.com>
2024-04-11 15:39:35 -0500

    Add files via upload

--- src/main/java/uta/cse3310/PlayerType.java
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -1,87 +1,19 @@
 package uta.cse3310;
+import org.java_websocket.WebSocket;
 
-public enum PlayerType {
-
-    NOPLAYER {
-        @Override
-        public int getScore() {
-            return 0;
-        }
-
-        @Override
-        public void updateScore(int newScore) {
-            // Do nothing for NOPLAYER
-        }
-    },
-
-    XPLAYER {
-        private String nickname;
-        private int playercurrScore;
-        private int playerID;
-
-        @Override
-        public int getScore() {
-            return playercurrScore;
-        }
-
-        @Override
-        public void updateScore(int newScore) {
-            playercurrScore += newScore;
-        }
-
-        public String getNickname() {
-            return nickname;
-        }
-
-        public void setNickname(String nickname) {
-            this.nickname = nickname;
-        }
-
-        public int getPlayerID() {
-            return playerID;
-        }
-
-        public void setPlayerID(int playerID) {
-            this.playerID = playerID;
-        }
-    },
-
-    OPLAYER {
-        private String nickname;
-        private int playercurrScore;
-        private int playerID;
-
-        @Override
-        public int getScore() {
-            return playercurrScore;
-        }
-
-        @Override
-        public void updateScore(int newScore) {
-            playercurrScore += newScore;
-        }
-
-        public String getNickname() {
-            return nickname;
-        }
-
-        public void setNickname(String nickname) {
-            this.nickname = nickname;
-        }
-
-        public int getPlayerID() {
-            return playerID;
-        }
-
-        public void setPlayerID(int playerID) {
-            this.playerID = playerID;
-        }
-    };
-
-    PlayerType() {
-    }
-
-    public abstract int getScore();
-
-    public abstract void updateScore(int newScore);
+public class PlayerType {
+  public String name;
+  public int score;
+  public int index;
+  public int gameNum;
+  public boolean isReady;
+  public WebSocket playerConn;
+  
+  PlayerType(String name, WebSocket newConn){
+    this.name = name;
+    this.isReady = false;
+    this.playerConn = newConn;
+    this.score = 0;
+  }
 }
+    
