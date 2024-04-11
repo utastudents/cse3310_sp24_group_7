683cb18e501692ea383eabceb2f908a668847d33
mtrey0220 <111877570+mtrey0220@users.noreply.github.com>
2024-04-11 13:17:04 -0500

    Update

--- src/main/java/uta/cse3310/HttpServer.java
+++ src/main/java/uta/cse3310/HttpServer.java
@@ -3,30 +3,55 @@ package uta.cse3310;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import net.freeutils.httpserver.HTTPServer;
 import net.freeutils.httpserver.HTTPServer.ContextHandler;
 import net.freeutils.httpserver.HTTPServer.FileContextHandler;
 import net.freeutils.httpserver.HTTPServer.Request;
 import net.freeutils.httpserver.HTTPServer.Response;
 import net.freeutils.httpserver.HTTPServer.VirtualHost;
-
 public class HttpServer {
 
-    // Directory where HTML files are stored \\
     private static final String HTML = "./html";
     int port = 8080;
     String dirname = HTML;
 
-    // Constructor to initialize the HttpServer \\
     public HttpServer(int portNum, String dirName) {
-        // Initialize port number and directory name \\
-        this.port = portNum;
-        this.dirname = dirName;
+        System.out.println("creating http server port " + portNum);
+        port = portNum;
+        dirname = dirName;
     }
 
-    
     public void start() {
-        // Implementation for starting the HTTP server added here
+        System.out.println("in httpd server start");
+        try {
+            // This File directory creates a File object based on the directory name specified
+            File dir = new File(dirname);
+            //Checks if the directory actually exists or is readable.
+            if (!dir.canRead())
+                throw new FileNotFoundException(dir.getAbsolutePath());
+
+
+            // set up server
+            HTTPServer server = new HTTPServer(port);
+
+            // Creates virtualHost
+            VirtualHost host = server.getVirtualHost(null); // default host
+            host.setAllowGeneratedIndex(true); // with directory index pages
+
+            //Sets an HTTP file context handler at the root URL. 
+            // 
+            // Possible to request files inside the directory wrongdir by using localhost:9080/filename
+            // Example: localhost:9080/index2.html will make a valid GET
+            // files within the same folder are visible to each other, this can be utilized 
+            // in the scenario that we implement multiple pages.
+            host.addContext("/", new FileContextHandler(dir));
+
+            server.start();
+            System.out.println("HTTPServer is listening on port " + port);
+        } catch (Exception e) {
+            System.err.println("error: " + e);
+        }
+
     }
+
 }
