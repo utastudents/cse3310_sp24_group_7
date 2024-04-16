f92409d949cc14aaa159c7e688e8398b36545e55
mtrey0220 <111877570+mtrey0220@users.noreply.github.com>
2024-04-15 21:45:42 -0500

    Update NicknameTest.java

--- src/test/java/uta/cse3310/NicknameTest.java
+++ src/test/java/uta/cse3310/NicknameTest.java
@@ -1,30 +1,30 @@
-// package uta.cse3310;
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.java_websocket.WebSocket;
-// import java.util.ArrayList;
+package uta.cse3310;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.java_websocket.WebSocket;
+import java.util.ArrayList;
 
-// class NicknameTest {
-//     private App app;
+class NicknameTest {
+    private App app;
 
-//     @BeforeEach
-//     void setUp() {
-//         app = new App(9880);
-//     }
+     @BeforeEach
+     void setUp() {
+       app = new App(9880);
+     }
 
-//     @Test
-//     void testIsNicknameUnique() {
-//         // Create sample players
-//         PlayerType player1 = new PlayerType("Player1", mock(WebSocket.class));
-//         PlayerType player2 = new PlayerType("Player2", mock(WebSocket.class));
+  @Test
+     void testIsNicknameUnique() {
+         // Create sample players
+         PlayerType player1 = new PlayerType("Player1", mock(WebSocket.class));
+         PlayerType player2 = new PlayerType("Player2", mock(WebSocket.class));
 
-//         // Add players to the playerList
-//         app.playerList.add(player1);
-//         app.playerList.add(player2);
+         // Add players to the playerList
+         app.playerList.add(player1);
+         app.playerList.add(player2);
 
-//         // unique nickname
-//         Assertions.assertTrue(app.isNicknameUnique("Player3"));
+        // unique nickname
+         Assertions.assertTrue(app.isNicknameUnique("Player3"));
 
 //         // non-unique nickname
 //         Assertions.assertFalse(app.isNicknameUnique("Player1"));
