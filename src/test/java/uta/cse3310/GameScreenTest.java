1482af41a24a7ee67e5c570a57f184cd815a9bf5
Abubakar Kassim <142273851+RSG0@users.noreply.github.com>
2024-04-18 16:57:34 -0500

    Update GameScreenTest.java

--- src/test/java/uta/cse3310/GameScreenTest.java
+++ src/test/java/uta/cse3310/GameScreenTest.java
@@ -1,23 +1,50 @@
-// package uta.cse3310;
-// import org.junit.jupiter.api.*;
+package uta.cse3310;
+import org.junit.jupiter.api.*;
 
-// import org.java_websocket.WebSocket;
-// import java.util.ArrayList;
+import org.java_websocket.WebSocket;
+import org.junit.Before;
+import org.junit.Test;
 
-// public class GameScreenTest 
-// {
-//     void setup()
-//     {
-//         App app = new App(9880); //
-//         // app.setOnOpen((WebSocket conn)->System.out.println("Connection opened"));
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+public class GameScreenTest 
+{
+    GameScreen game = new GameScreen();
+
+    @BeforeEach
+    public void verifySetup()
+    {
+        App app = new App(9880); //
+        // app.setOnOpen((WebSocket conn)->System.out.println("Connection opened"));
+        /*
+         * Need to finalize cpde
+         */
+        game.StartGame();
+        assertTrue(game.isOpen);
+    }
+    @Test
+    public void testStartGame()
+    {
+
+        assertTrue(false);
+    }
+    @Test
+    public void verfiyTimer() // is this trivial
+    {
+        GameScreen game = new GameScreen();
         
-//         GameScreen game = new GameScreen();
-//         if (game.isOpen != true)
-//         {
-//             System.out.println("GameScreen will not open");
+        assertTrue(false);
+    }
+    @Test
+    public void testHighlightingWord()
+    {
 
-//         }
-
-
-//     }
-// }
+    }
+    @Test
+    public void testRestartButton()
+    {
+        
+    }
+}
