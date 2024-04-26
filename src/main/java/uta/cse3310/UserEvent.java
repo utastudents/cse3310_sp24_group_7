cba8cbec2a9b3e9ac6b01386dcecde11aee718d6
DeanTrammell <159579818+DeanTrammell@users.noreply.github.com>
2024-04-26 13:21:25 -0500

    Update UserEvent.java
    added new user events. made by: dean trey, ebenezer

--- src/main/java/uta/cse3310/UserEvent.java
+++ src/main/java/uta/cse3310/UserEvent.java
@@ -1,7 +1,13 @@
 package uta.cse3310;
+// User events are sent from the webpage to the server
 
 public class UserEvent {
-    int GameId;
-    PlayerType PlayerIdx;
-    int Button;
+    PlayerType PlayerIdx; // either an XPLAYER or an OPLAYER
+
+    int GameId; // the game ID on the server
+    //add LobbyId ?
+    int Button; // button number from 0 to 8
+    int GridRow; //row index of grid letter pushed
+    int GridColumn; //column index of grid letter pushed
+    String Msg; // chat message sent
 }
