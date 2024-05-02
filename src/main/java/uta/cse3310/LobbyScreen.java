6ea4050d17e292eb8f1b6c0e591c6f15bf32a52a
mtrey0220 <111877570+mtrey0220@users.noreply.github.com>
2024-05-01 21:35:53 -0500

    Lobby Final

--- src/main/java/uta/cse3310/LobbyScreen.java
+++ src/main/java/uta/cse3310/LobbyScreen.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class LobbyScreen {
     private List<Player> players;
     private int lobbyId;
-    private ChatBox chatBox;
+    //private ChatBox chatBox;
     private int playersJoined;
 
     public LobbyScreen() {
         players = new ArrayList<>();
         playersJoined = 0;
-        chatBox = new ChatBox();
+        //chatBox = new ChatBox();
     }
 
     public void addPlayer(Player player) {
@@ -24,7 +24,17 @@ public class LobbyScreen {
             System.out.println("Lobby is full. Cannot add more players.");
         }
     }
-    
+
+    public void clearTable() {
+        players.clear();
+        playersJoined = 0;
+        System.out.println("Lobby Table cleared. Starting new Game!");
+    }
+
+    public int getGameId() {
+        return lobbyId;
+    }
+
 
     public void update(UserEvent U) {
         System.out.println("Received update from Player " + U.PlayerIdx + ": " + U.Msg);
